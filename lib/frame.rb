04119cb9738c06e8242ab4a88dac5bbd728e020e
Alice Wood <alicesophiewood@gmail.com>
2023-05-26 15:59:41 +0100

    rewriting frame so it initializes with the roll values

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,22 +1,12 @@
 class Frame
-  attr_accessor :roll_results, :frame_points
+  attr_accessor :rolls, :calculate_frame_points
 
-  def initialize
-    @roll_results = []
-  end
-
-  def rolls(roll1, roll2)
-    @roll_results << roll1
-    if roll1 == 10
-      roll2 = 0
-      @roll_results << roll2
-    else 
-      @roll_results << roll2
-    end
+  def initialize(roll1, roll2)
+    @roll_results = [roll1, roll2]
   end
 
   def calculate_frame_points
-    @roll_results.reduce(:+)
+    @roll_results == []? 0 : @roll_results.reduce(:+)
   end
 
   def is_strike?
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,42 +1,30 @@
 require 'frame'
 
 RSpec.describe 'Frame' do
-  context 'it initializes and constructs' do
-    it 'initializes with no roll points or roll results' do
-      frame = Frame.new
-      expect(frame.roll_results).to eq []
-    end
-  end
-
   context 'one frame' do
     it 'returns 0 when 2 gutter balls are rolled' do
-      frame = Frame.new
-      frame.rolls(0, 0)
+      frame = Frame.new(0, 0)
       expect(frame.calculate_frame_points).to eq 0
     end
 
     it 'returns 6 for a 4 and a 2 rolled - regular roll' do
-      frame = Frame.new
-      frame.rolls(4, 2)
+      frame = Frame.new(4, 2)
       expect(frame.calculate_frame_points).to eq 6
     end
 
     it 'returns 10 for a spare' do
-      frame = Frame.new
-      frame.rolls(4, 6)
+      frame = Frame.new(4, 6)
       expect(frame.calculate_frame_points).to eq 10
     end
     
     it 'recognises if the first roll was a strike' do
-      frame = Frame.new
-      frame.rolls(10, 0)
+      frame = Frame.new(10, 0)
       expect(frame.calculate_frame_points).to eq 10
       expect(frame.is_strike?).to eq true
     end
 
     it 'recognises if the rolls made a spare' do
-      frame = Frame.new
-      frame.rolls(4, 6)
+      frame = Frame.new(4, 6)
       expect(frame.calculate_frame_points).to eq 10
       expect(frame.is_spare?).to eq true
     end
